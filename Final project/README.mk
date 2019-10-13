7e5b633fcdab0ed2560f6c80fb5313404be558e8
alifele <ali.fele@gmail.com>
2019-10-14 00:54:56 +0330

    percolation update

---
+++ Final project/README.mk
@@ -0,0 +1,11 @@
+this folder is for my final project.
+
+I am going to work on the cellular potts model
+
+I may print it on the takaneh with elahe.
+
+
+
+this folder also will contain the draft of the takaneh work
+
+
